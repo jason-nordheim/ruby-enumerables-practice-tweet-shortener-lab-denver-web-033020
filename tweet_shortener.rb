b524e57e0edc99972fc4948b16b581e08eee172b
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 17:20:16 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,5 +13,6 @@ a = "Hey guys, can anyone teach me how to be cool? I really want to be the best 
 b = "Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"
 
 def bulk_tweet_shortener tweets
-  tweets.map { |x| word_substituter(x)  }
+  tweets.each { |x| puts word_substituter(x)  }
+
 end
