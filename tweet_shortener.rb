80fca9f4fff05e947e20962259afd975aa3f5b2f
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 17:16:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,10 +1,10 @@
 # Write your code here.
 
 def word_substituter words
-  words = words.gsub("to\i","2") # substitute to for 2
-  words = words.gsub("be\i","b") # shorten "be" to "b"
-  words = words.gsub("at\i","@") # shorten "at" to "@"
-  words = words.gsub("you\i","u")
+  words = words.gsub("to","2") # substitute to for 2
+  words = words.gsub("be","b") # shorten "be" to "b"
+  words = words.gsub("at","@") # shorten "at" to "@"
+  words = words.gsub("you","u")
 end
 
 
