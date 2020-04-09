5fb8c7b57487aee09f89c7e4a80233699e693237
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 17:14:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,6 +1,11 @@
 # Write your code here.
 
 def word_substituter words
-  pattern = ""
-  word.gsub()
+  words.gsub("to\i","2") # substitute to for 2
+  words.gsub("be\i","b") # shorten "be" to "b"
 end
+
+
+a = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+
+b = "Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"
