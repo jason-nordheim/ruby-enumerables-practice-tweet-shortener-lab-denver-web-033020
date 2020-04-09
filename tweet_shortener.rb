018cb69d9fa380f2d01250a6020a929b1d6ba0ac
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 17:27:29 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -21,7 +21,7 @@ def bulk_tweet_shortener tweets
 end
 
 def selective_tweet_shortener tweet
-  if tweet.length == 140
+  if tweet.length > 140
     word_substituter(tweet).slice(0,140)
   else
     tweet
