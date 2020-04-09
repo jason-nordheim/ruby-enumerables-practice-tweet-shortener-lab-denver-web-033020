751094c2b8ba02c245d119e3130bd0135787012c
Jason Nordheim <jason.nordheim@gmail.com>
2020-04-09 17:19:00 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -11,3 +11,6 @@ end
 a = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
 b = "Hey guys, can anyone teach me how 2 b cool? I really want 2 b the best @ everything, u know what I mean? Tweeting is super fun u guys!!!!"
+
+def bulk_tweet_shortener tweets
+end 
